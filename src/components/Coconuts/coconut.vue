0d66d29d48ffda93bbd2fc0948c4c374ccbcd7c2
kinoko0841 <mengtianshu0841@gmail.com>
2017-03-30 22:41:02 -0700

    ini

---
+++ src/components/Coconuts/coconut.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="coconut">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'coconut',
+  data () {
+    return {
+      msg: 'This is a coconut.'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '@/components/Hello'
+// import Coconut from '@/components/Coconuts/Coconut'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Hello',
+      component: Hello
+    }
+    // {
+    //   path: '/',
+    //   name: 'Coconut',
+    //   component: Coconut
+    // }
+  ]
+})
