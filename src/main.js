3f24a414c2a1b6d119bddf3cf326539934e1dda0
kinoko0841 <mengtianshu0841@gmail.com>
2017-04-03 12:10:31 -0700

    import and source element.ui default theme

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-default/index.css'
+import '../theme/index.css'
+// import 'element-ui/lib/theme-default/index.css'
 import router from './router'
 
 Vue.config.productionTip = false
