4cc9be8ba89a12a6aa85b849ed2a395980ff9fe6
kinoko0841 <mengtianshu0841@gmail.com>
2017-04-04 15:10:13 -0700

    nav demo

--- src/components/Coconuts/mentor.vue
+++ src/components/Coconuts/mentor.vue
@@ -2,10 +2,10 @@
   <div class = "mentor">
     <h1>{{ msg }}</h1>
     <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="用户管理" name="first">用户管理</el-tab-pane>
-      <el-tab-pane label="配置管理" name="second">配置管理</el-tab-pane>
-      <el-tab-pane label="角色管理" name="third">角色管理</el-tab-pane>
-      <el-tab-pane label="定时任务补偿" name="fourth">定时任务补偿</el-tab-pane>
+      <el-tab-pane label="课程管理" name="课程管理">课程管理</el-tab-pane>
+      <el-tab-pane label="服务管理" name="服务管理">服务管理</el-tab-pane>
+      <el-tab-pane label="文件管理" name="文件管理">文件管理</el-tab-pane>
+      <el-tab-pane label="payments" name="payments">payments</el-tab-pane>
     </el-tabs>
   </div>
 </template>
@@ -15,7 +15,7 @@ export default {
   data () {
     return {
       msg: 'I am Mentor',
-      activeName: 'second'
+      activeName: '课程管理'
     }
   },
   methods: {
